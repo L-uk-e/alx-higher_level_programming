cc6556a1a7453ae27b5ca664c0834e4ba4d50504
Luke Mwangi <lukamwangi.k@gmail.com>
2022-12-19 16:22:34 +0300

    Mod Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    word = my_string
-    length = len(word)
-    s = 0
+    word = ""
 
-    while s < length:
-        if word[s] == 'c' or word[s] == 'C':
-            word = word[:s] + word[s + 1:]
-            length -= 1
-        s += 1
-
+    for w in my_string:
+        if w != "c" and w != "C":
+            word = word + w
     return word
